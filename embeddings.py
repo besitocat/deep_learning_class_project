0aac83d0b7757efb1a75160e1133f421462b11a4
Evita Bakopoulou <ebakopou@uci.edu>
2018-03-07 18:11:05 -0800

    minor updates

--- embeddings.py
+++ embeddings.py
@@ -11,14 +11,16 @@ from tqdm import tqdm
 import codecs
 import numpy as np
 from gensim.models.wrappers import FastText
-#from gensim.models import FastText
+
 
 glove_file_english_only_100d = "glove/100d_glove_english_only.txt"
-fast_text_file = "../fasttext/wiki.simple"
+fast_text_file = "../fasttext/wiki.simple" #the binary version of FFtext
+
 
 def load_to_gensim(glove_file_english_only):
     model = KeyedVectors.load_word2vec_format(glove_file_english_only_100d, binary=False) #GloVe Model - not updatable
     return model
+
 
 def get_glove_embed(list_of_words):
     word2vec = load_to_gensim(glove_file_english_only_100d) 
@@ -30,17 +32,26 @@ def get_glove_embed(list_of_words):
     return glove_word_dict
 
 
-def get_fasttext_embed(word):
+def get_fasttext_embed_for_word(word):
     ff_model = FastText.load_fasttext_format(fast_text_file)
     return ff_model[word]
 
 
+def get_fasttext_embed(list_of_words):
+    words_embed_dict = dict()
+    ff_model = FastText.load_fasttext_format(fast_text_file)
+    for w in list_of_words:
+        words_embed_dict[w] = ff_model[w]
+    return words_embed_dict
+
+
 def main():
     list_of_words = ['hello', 'how', 'are', 'oy', 'y444']
-    get_glove_embed(list_of_words)
-    print get_glove_embed
-    ff_embedding= get_fasttext_embed('gegaegag')
-    print ff_embedding('girl')
-
+    glove_embed = get_glove_embed(list_of_words)
+    fftext_embed = get_fasttext_embed(list_of_words)
+#    print glove_embed
+#    print get_fasttext_embed_for_word('gegaegag')
+#    print fftext_embed
+    
 if __name__ == "__main__":
     main()
