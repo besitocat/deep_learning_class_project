73c4c30409f70fc377be936b079778416aa59fd8
Sameera <sghayyur@uci.edu>
2018-03-07 16:01:29 -0800

    fasttext embeddings

---
+++ embs.py
@@ -0,0 +1,21 @@
+from tqdm import tqdm
+import codecs
+import numpy as np
+
+
+def loadFastTextEmb():
+    print('loading word embeddings...')
+    embeddings_index = {}
+    f = codecs.open('wiki.simple.vec', encoding='utf-8')
+    for line in tqdm(f):
+        values = line.rstrip().rsplit(' ')
+        fword = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        embeddings_index[fword] = coefs
+    return embeddings_index
+    print('found %s word vectors' % len(embeddings_index))
+    
+if __name__ == "__main__":
+    embeddingDic= loadFastTextEmb()
+    embedding = embeddingDic['hello']
+    print embedding
